ff53973fc9d8b136badf17522eb04dd6e974dd71
Arto Javanainen <arto.javanainen@jyu.fi>
2026-04-08 10:45:19 +0300

    fix: korjaa polkumääritys

--- src/annosennustettavuusmalli/preprocessing/luokat.py
+++ src/annosennustettavuusmalli/preprocessing/luokat.py
@@ -13,6 +13,7 @@ from pathlib import Path
 from typing import Optional
 
 import pydicom
+from loguru import logger  # type: ignore
 
 # Keskitetty juuripolku:
 BASE_DIR = Path(r"C:\Users\User01\GRADU\Aineisto")
@@ -160,11 +161,22 @@ class Patient:
 class AllPatients:
     original_root: Path
     processed_root: Path
+    base_dir: Path = BASE_DIR
 
     def __post_init__(self):
-        self.original_root = Path(self.original_root).resolve()
-        self.processed_root = Path(self.processed_root).resolve()
+        self.original_root = self.ensure_paths(self.original_root)
+        self.processed_root = self.ensure_paths(self.processed_root)
         self._patients = self._find_patients()
+
+    def ensure_paths(self, path: Path):
+        """Varmistaa, että annettu polku on olemassa, muuten luo sen."""
+        logger.debug(f"Ensuring path exists: {path}")
+
+        output_path = self.base_dir / path.name
+
+        if not output_path.exists():
+            output_path.mkdir(parents=True, exist_ok=True)
+        return output_path
 
     def _find_patients(self) -> list[Patient]:
         patient_dirs = self.original_root.glob("Patient*")
